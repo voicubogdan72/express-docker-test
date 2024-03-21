97805d5cd0aa4c6801893f4f6e1ee34a0b2fd26f
Voicu Bogdan <voicubogdan72@gmail.com>
2024-03-21 18:03:35 +0200

    sadasddsa

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,16 @@ const app: Express = express();
 const port = process.env.PORT || 7777;
 
 app.get("/", async (req: Request, res: Response) => {
-  mongoose
-    .connect("mongodb://localhost:27017/example", {})
-    .then(() => {
-      console.log("Connected to MongoDB");
-      res.send("Ok connected!");
-    })
-    .catch((error) => {
-      console.error("Error connecting to MongoDB", error);
-    });
+  res.send("Without db");
+  // mongoose
+  //   .connect("mongodb://localhost:27017/example", {})
+  //   .then(() => {
+  //     console.log("Connected to MongoDB");
+  //     res.send("Ok connected!");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error connecting to MongoDB", error);
+  //   });
   // try {
   //   const db = await mongoose.connect("mongodb://localhost:27017/example");
   //   if (db) {
